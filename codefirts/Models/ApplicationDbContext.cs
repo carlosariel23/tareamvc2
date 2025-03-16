8825e259c275a0f25a57fc302eeb20aa40414084
Ariel <Ariel@DESKTOP-JK4J511>
2025-03-16 17:31:39 -0600

    se creo la base de datos y se creo la clase en este caso Empleado

---
+++ codefirts/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace codefirts.Models
+{
+
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Empleado> Empleados { get; set; }
+        public DbSet<Alumno> Alumnos { get; set; }
+    }
+
+}
---
+++ codefirts/Models/Empleado.cs
@@ -0,0 +1,53 @@
+﻿namespace codefirts.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Empleado
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        [StringLength(15)]
+        public string Telefono { get; set; }
+
+        [StringLength(10)]
+        public string DUI { get; set; }
+
+        [StringLength(200)]
+        public string Direccion { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+
+    public class Alumno
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Codigo { get; set; }
+
+        [StringLength(200)]
+        public string Direccion { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public DateTime FechaNacimiento { get; set; }
+
+        public short YearEstudio { get; set; }
+    }
+
+}
